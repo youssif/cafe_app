6af856f5b8aaafea88cb25a522e211a4657b9d80
Youssif <abdulyoussif@gmail.com>
2013-07-09 14:20:18 -0700

    Got things to post to server, but it all worked out

--- public/square.js
+++ public/square.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   $('form').
   	on('change', 'select', function(){
   		alert("my my, how you've changed");
-  		$('form').append('<select>' + selected + '</select><br />');
+  		$('form div:first').append('<select>' + selected + '</select><br />');
   		item_count++;
   		$('#drinks').text(item_count);
   		total_cost = total_cost + Number($(this).find(':selected').attr('data-price'));
@@ -18,5 +18,25 @@ $(document).ready(function() {
   		$('#cost').text("$"+(total_cost/100).toFixed(2));
   	});
 
+//when the checkout button is clicked, we need to post 
+  $(".button").click(function(event){
+  	event.preventDefault();
+  	$.post('/shop', total_cost, function(responsetext) {
+		$('#order_form').html("<h2>" + responsetext + "<h2>");
+  	});
+
+  	// $.post(shop, info to send, what is a function to respond)
+
+  	// $.ajax({
+  	// 	type: "POST",
+  	// 	url: "/shop",
+  	// 	data: total_cost,
+  	// 	success: function(event){
+  	// 		$('#order_form').html("<div id = 'message'></div>");
+  	// 		$('#message').html("<h2>Your order has been submitted!<h2>")
+  	// 		.append("<p>Your final cost is $"+total_cost+"</p>")
+  	// 	}
+  	// });
+   });
 });
 
